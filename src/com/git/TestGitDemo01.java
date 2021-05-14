d782db5f4c7f617aee45ba7b6dd02a1226c6e4a3
tha <1272064499@qq.com>
2021-05-14 17:24:40 +0800

    将项目提交到版本库

---
+++ src/com/git/TestGitDemo01.java
@@ -0,0 +1,7 @@
+package com.git;
+
+public class TestGitDemo01 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/git/TestGitDemo02.java
@@ -0,0 +1,7 @@
+package com.git;
+
+public class TestGitDemo02 {
+    public static void main(String[] args) {
+
+    }
+}
